eae1932891b1b69383e91a1b8083690a320cfb50
Karina Ariga de Oliveira <kariariga@hotmail.com>
2022-11-11 17:36:38 -0300

    Aula 03 - Implementando Dark Mode com ContextAPI

--- pages/index.js
+++ pages/index.js
@@ -1,102 +1,102 @@
 import React from 'react';
 import config from '../config.json';
 import styled from 'styled-components';
-import { CSSReset } from '../src/components/CSSReset';
 import Menu from '../src/components/Menu';
 import { StyledTimeline } from '../src/components/Timeline';
 
 function HomePage() {
-	const [valorDoFiltro, setValorDoFiltro] = React.useState('');
+    const [valorDoFiltro, setValorDoFiltro] = React.useState('');
 
-	return (
-		<>
-			<CSSReset />
-			<div
+    return (
+        <>
+            <div
 				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					flex: 1,
-				}}
+                display: 'flex',
+                flexDirection: 'column',
+                flex: 1,
+            }}
 			>
-				<Menu
+                <Menu
 					valorDoFiltro={valorDoFiltro}
 					setValorDoFiltro={setValorDoFiltro}
 				/>
-				<Header />
-				<Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
-					Conteúdo
-				</Timeline>
-			</div>
-		</>
-	);
+                <Header />
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
+                    Conteúdo
+                </Timeline>
+            </div>
+        </>
+    );
 }
 
 export default HomePage;
 
 const StyledHeader = styled.div`
-	img {
-		width: 80px;
-		height: 80px;
-		border-radius: 50%;
-	}
-	.user-info {
-		display: flex;
-		align-items: center;
-		width: 100%;
-		padding: 16px 32px;
-		gap: 16px;
-	}
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
 `;
 const StyledBanner = styled.div`
-	background-color: blue;
-	background-image: url(${({ bg }) => bg});
-	/* background-image: url(${config.bg}); */
-	height: 230px;
+    background-color: blue;
+    background-image: url(${({ bg }) => bg});
+    /* background-image: url(${config.bg}); */
+    height: 230px;
 `;
 function Header() {
-	return (
-		<StyledHeader>
-			<StyledBanner bg={config.bg} />
-			<section className='user-info'>
-				<img src={`https://github.com/${config.github}.png`} />
-				<div>
-					<h2>{config.name}</h2>
-					<p>{config.job}</p>
-				</div>
-			</section>
-		</StyledHeader>
-	);
+    return (
+        <StyledHeader>
+            <StyledBanner bg={config.bg} />
+            <section className='user-info'>
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>{config.name}</h2>
+                    <p>{config.job}</p>
+                </div>
+            </section>
+        </StyledHeader>
+    );
 }
 
 function Timeline({ searchValue, ...propriedades }) {
-	const playlistNames = Object.keys(propriedades.playlists);
- 
-	return (
-		<StyledTimeline>
-			{playlistNames.map((playlistName) => {
-				const videos = propriedades.playlists[playlistName];
-				return (
-					<section key={playlistName}>
-						<h2>{playlistName}</h2>
-						<div>
-							{videos
-								.filter((video) => {
-									const titleNormalized = video.title.toLowerCase();
-									const searchValueNormalized = searchValue.toLowerCase();
-									return titleNormalized.includes(searchValueNormalized);
-								})
-								.map((video) => {
-									return (
-										<a key={video.url} href={video.url}>
-											<img src={video.thumb} />
-											<span>{video.title}</span>
-										</a>
-									);
-								})}
-						</div>
-					</section>
-				);
-			})}
-		</StyledTimeline>
-	);
+    const playlistNames = Object.keys(propriedades.playlists);
+
+    return (
+        <StyledTimeline>
+            {playlistNames.map((playlistName) => {
+                const videos = propriedades.playlists[playlistName];
+                return (
+                    <section key={playlistName}>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {videos
+                                .filter((video) => {
+                                    const titleNormalized = video.title.toLowerCase();
+                                    const searchValueNormalized = searchValue.toLowerCase();
+                                    return titleNormalized.includes(searchValueNormalized);
+                                })
+                                .map((video) => {
+                                    return (
+                                        <a key={video.url} href={video.url}>
+                                            <img src={video.thumb} />
+                                            <span>{video.title}</span>
+                                        </a>
+                                    );
+                                })}
+                        </div>
+                    </section>
+                );
+            })}
+        </StyledTimeline>
+    );
 }
---
+++ src/components/Menu/components/ColorMode.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const ColorModeContext = React.createContext({
+	mode: '',
+	setMode: () => {
+		alert('Você precisa me configurar primeiro!');
+	},
+	toggleMode: () => {
+		alert('Você precisa me configurar primeiro!');
+	},
+});
+
+export default function ColorModeProvider(props) {
+	const [mode, setMode] = React.useState(props.initialMode);
+
+	function toggleMode() {
+		if (mode === 'dark') setMode('light');
+		if (mode === 'light') setMode('dark');
+	}
+
+	return (
+		<ColorModeContext.Provider value={{ mode: mode, toggleMode: toggleMode }}>
+			{props.children}
+		</ColorModeContext.Provider>
+	);
+}
